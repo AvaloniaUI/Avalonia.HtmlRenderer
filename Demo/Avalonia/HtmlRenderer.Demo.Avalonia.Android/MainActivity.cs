46f2e6e01baccef8ac7ad5d4d9d511737d3fc883
Max Katz <maxkatz6@outlook.com>
2023-07-10 19:35:55 -0400

    Update to stable

--- Demo/Avalonia/HtmlRenderer.Demo.Avalonia.Android/MainActivity.cs
+++ Demo/Avalonia/HtmlRenderer.Demo.Avalonia.Android/MainActivity.cs
@@ -1,10 +1,19 @@
 ﻿using Android.App;
 using Android.Content.PM;
+using Avalonia;
 using Avalonia.Android;
 
 namespace HtmlRenderer.Demo.Avalonia.Android;
 
-[Activity(Label = "HtmlRenderer.Demo.Avalonia.Android", Theme = "@style/MyTheme.NoActionBar", Icon = "@drawable/icon", LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize)]
-public class MainActivity : AvaloniaMainActivity
-{
+[Activity(
+    Label = "HtmlRenderer.Demo.Avalonia.Android",
+    Theme = "@style/MyTheme.NoActionBar",
+    Icon = "@drawable/icon",
+    MainLauncher = true,
+    ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
+public class MainActivity : AvaloniaMainActivity<App>{
+    protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
+    {
+        return base.CustomizeAppBuilder(builder);
+    }
 }
