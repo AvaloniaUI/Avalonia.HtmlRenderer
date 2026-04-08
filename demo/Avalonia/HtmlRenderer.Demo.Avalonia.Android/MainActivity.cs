77de6a81422a780af6f96d5fa2e555c9385836b6
Max Katz <maxkatz6@outlook.com>
2026-04-07 20:36:53 -0700

    Recreate templates

--- demo/Avalonia/HtmlRenderer.Demo.Avalonia.Android/MainActivity.cs
+++ demo/Avalonia/HtmlRenderer.Demo.Avalonia.Android/MainActivity.cs
@@ -11,10 +11,6 @@ namespace HtmlRenderer.Demo.Avalonia.Android;
     Icon = "@drawable/icon",
     MainLauncher = true,
     ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
-public class MainActivity : AvaloniaMainActivity<App>
+public class MainActivity : AvaloniaMainActivity
 {
-    protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
-    {
-        return base.CustomizeAppBuilder(builder);
-    }
 }
